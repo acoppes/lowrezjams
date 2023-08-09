0d8f8a5ffa83342ab227e106519e1cc0808d227c
Ariel Coppes <ariel.coppes@gmail.com>
2023-08-08 23:50:17 -0300

    moved limit vertical movement config to system changed camera offset, added more playable area, reduced ufo height, reduced beam size

--- Abduction101/Assets/Abduction101/Systems/LimitVerticalMovementSystem.cs
+++ Abduction101/Assets/Abduction101/Systems/LimitVerticalMovementSystem.cs
@@ -7,6 +7,9 @@ namespace Abduction101.Systems
 {
     public class LimitVerticalMovementSystem : BaseSystem, IEcsRunSystem
     {
+        public float minZ = -0.5f;
+        public float maxZ = 1.5f;
+        
         readonly EcsFilterInject<Inc<PositionComponent, LimitMovementComponent>, Exc<DisabledComponent>> filter = default;
         
         public void Run(EcsSystems systems)
@@ -18,14 +21,14 @@ namespace Abduction101.Systems
 
                 var p = position.value;
 
-                if (p.z > limitMovement.maxZ)
+                if (p.z > maxZ + limitMovement.maxZ)
                 {
-                    p.z = limitMovement.maxZ;
+                    p.z = maxZ + limitMovement.maxZ;
                 }
                 
-                if (p.z < limitMovement.minZ)
+                if (p.z < minZ + limitMovement.minZ)
                 {
-                    p.z = limitMovement.minZ;
+                    p.z = minZ + limitMovement.minZ;
                 }
 
                 position.value = p;
