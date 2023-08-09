71893ad7735220ef124fc3f701738a974b81c79b
Ariel Coppes <ariel.coppes@gmail.com>
2023-08-09 15:27:27 -0300

    mockup for circular world idea using multiple cameras and teleporting objects

---
+++ Abduction101/Assets/Development/Abduction101/TestTeleportObjectBounds.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTeleportObjectBounds : MonoBehaviour
+{
+    public float minX, maxX;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var p = transform.position;
+        if (p.x > maxX)
+        {
+            p.x = minX;
+        }
+
+        if (p.x < minX)
+        {
+            p.x = maxX;
+        }
+
+        transform.position = p;
+    }
+}
