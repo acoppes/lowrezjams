17dc5bc9f72b52810badf376dcbd0af29f11543e
Ariel Coppes <ariel.coppes@gmail.com>
2023-08-09 14:01:00 -0300

    working on recoloring units

---
+++ Abduction101/Assets/Abduction101/Data/ColorSet.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Abduction101.Data
+{
+    [CreateAssetMenu(menuName="Gemserk/ColorSet")]
+    public class ColorSet : ScriptableObject
+    {
+        public Color[] colors;
+    }
+}
---
+++ Abduction101/Assets/Development/Abduction101/GenerateLutTextureTest.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using MyBox;
+using UnityEngine;
+
+public class GenerateLutTextureTest : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+
+    public Color[] testLutColors;
+    
+    public Texture2D lutTexture;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        if (lutTexture != null)
+        {
+            DestroyImmediate(lutTexture);
+        }
+    }
+
+    [ButtonMethod]
+    void RegenerateLut()
+    {
+        if (lutTexture != null)
+        {
+            DestroyImmediate(lutTexture);
+            lutTexture = null;
+        }
+
+        if (spriteRenderer == null)
+        {
+            return;
+        }
+
+        lutTexture = new Texture2D(testLutColors.Length, 1, TextureFormat.ARGB32, false);
+        lutTexture.filterMode = FilterMode.Point;
+        lutTexture.wrapMode = TextureWrapMode.Clamp;
+        
+        lutTexture.SetPixels(testLutColors);
+        lutTexture.Apply();
+        
+        // copy of the material
+        // material = spriteRenderer.material;
+        var propertyBlock = new MaterialPropertyBlock();
+        spriteRenderer.GetPropertyBlock(propertyBlock, 0);
+        propertyBlock.SetTexture("_MainTex", spriteRenderer.sprite.texture);
+        propertyBlock.SetTexture("_LutTex", lutTexture);
+        spriteRenderer.SetPropertyBlock(propertyBlock);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
