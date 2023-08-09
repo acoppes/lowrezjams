51955196bbc2903c4560f8e27f86c5155d04c8c2
Ariel Coppes <ariel.coppes@gmail.com>
2023-08-09 14:28:37 -0300

    added skin coloring system for units

---
+++ Abduction101/Assets/Abduction101/Components/UnitColorSetComponentDefinition.cs
@@ -0,0 +1,38 @@
+﻿using Abduction101.Data;
+using Gemserk.Leopotam.Ecs;
+using UnityEngine;
+
+namespace Abduction101.Components
+{
+    public struct ColorSetComponent : IEntityComponent
+    {
+        public ColorSet hair;
+        public ColorSet skin;
+        public ColorSet body;
+
+        public Texture2D lutTexture;
+        public MaterialPropertyBlock materialPropertyBlock;
+    }
+    
+    public class UnitColorSetComponentDefinition : ComponentDefinitionBase
+    {
+        public ColorSet hair;
+        public ColorSet skin;
+        public ColorSet body;
+        
+        public override string GetComponentName()
+        {
+            return nameof(ColorSetComponent);
+        }
+
+        public override void Apply(World world, Entity entity)
+        {
+            world.AddComponent(entity, new ColorSetComponent()
+            {
+                hair = hair,
+                skin = skin,
+                body = body
+            });
+        }
+    }
+}
---
+++ Abduction101/Assets/Abduction101/Systems/ColorSetSystem.cs
@@ -0,0 +1,87 @@
+﻿using Abduction101.Components;
+using Game.Components;
+using Gemserk.Leopotam.Ecs;
+using Gemserk.Utilities;
+using Leopotam.EcsLite;
+using Leopotam.EcsLite.Di;
+using UnityEngine;
+
+namespace Abduction101.Systems
+{
+    public class ColorSetSystem : BaseSystem, IEcsRunSystem, IEntityDestroyedHandler
+    {
+        readonly EcsFilterInject<Inc<ModelComponent, ColorSetComponent>, Exc<DisabledComponent>> filter = default;
+        
+        // public void OnEntityCreated(World world, Entity entity)
+        // {
+        //     if (entity.Has<ColorSetComponent>() && entity.Has<ModelComponent>())
+        //     {
+        //         // return model to original setting
+        //         ref var model = ref entity.Get<ModelComponent>()
+        //         ref var colorSet = ref filter.Pools.Inc2.Get(e);
+        //
+        //         if (colorSet.lutTexture == null)
+        //         {
+        //             
+        //         }
+        //         
+        //     }
+        // }
+        
+        public void OnEntityDestroyed(World world, Entity entity)
+        {
+            if (entity.Has<ColorSetComponent>() && entity.Has<ModelComponent>())
+            {
+                // return model to original setting
+                ref var model = ref entity.Get<ModelComponent>();
+                ref var colorSet = ref entity.Get<ColorSetComponent>();
+
+                if (colorSet.lutTexture != null)
+                {
+                    Destroy(colorSet.lutTexture);
+                    colorSet.lutTexture = null;
+                }
+
+                // if (model.instance != null)
+                // {
+                //     model.instance.spriteRenderer.sharedMaterial = 
+                // }
+            }
+        }
+        
+        public void Run(EcsSystems systems)
+        {
+            foreach (var e in filter.Value)
+            {
+                ref var model = ref filter.Pools.Inc1.Get(e);
+                ref var colorSet = ref filter.Pools.Inc2.Get(e);
+
+                if (colorSet.lutTexture == null && model.instance != null && model.instance.spriteRenderer != null)
+                {
+                    var lutColors = new Color[]
+                    {
+                        colorSet.body.colors.Random(),
+                        colorSet.skin.colors.Random(),
+                        colorSet.hair.colors.Random()
+                    };
+                    
+                    colorSet.lutTexture = new Texture2D(lutColors.Length, 1, TextureFormat.ARGB32, false);
+                    colorSet.lutTexture.filterMode = FilterMode.Point;
+                    colorSet.lutTexture.wrapMode = TextureWrapMode.Clamp;
+        
+                    colorSet.lutTexture.SetPixels(lutColors);
+                    colorSet.lutTexture.Apply();
+                    
+                    colorSet.materialPropertyBlock = new MaterialPropertyBlock();
+                    model.instance.spriteRenderer.GetPropertyBlock(colorSet.materialPropertyBlock, 0);
+                    // colorSet.materialPropertyBlock.SetTexture("_MainTex", model.instance.spriteRenderer.sprite.texture);
+                    colorSet.materialPropertyBlock.SetTexture("_LutTex", colorSet.lutTexture);
+                    model.instance.spriteRenderer.SetPropertyBlock(colorSet.materialPropertyBlock);
+                }
+            }
+        }
+
+
+
+    }
+}
