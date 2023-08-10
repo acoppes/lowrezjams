310dfd33fd1c8666c089ac13606ad62d148b49a8
Ariel Coppes <ariel.coppes@gmail.com>
2023-08-10 16:42:20 -0300

    camera confiner entity follows player but only x
    added system to copy position from entity to gameobject

---
+++ Abduction101/Assets/Abduction101/Systems/CopyPositionToGameObjectSystem.cs
@@ -0,0 +1,28 @@
+﻿using Abduction101.Components;
+using Game.Components;
+using Game.Systems;
+using Gemserk.Leopotam.Ecs;
+using Gemserk.Leopotam.Ecs.Components;
+using Gemserk.Utilities;
+using Leopotam.EcsLite;
+using Leopotam.EcsLite.Di;
+using UnityEngine;
+
+namespace Abduction101.Systems
+{
+    public class CopyPositionToGameObjectSystem : BaseSystem, IEcsRunSystem
+    {
+        readonly EcsFilterInject<Inc<PositionComponent, GameObjectComponent>, Exc<DisabledComponent>> filter = default;
+        
+        public void Run(EcsSystems systems)
+        {
+            foreach (var e in filter.Value)
+            {
+                ref var position = ref filter.Pools.Inc1.Get(e);
+                ref var gameObjectComponent = ref filter.Pools.Inc2.Get(e);
+
+                gameObjectComponent.gameObject.transform.position = GamePerspective.ConvertFromWorld(position.value);
+            }
+        }
+    }
+}
