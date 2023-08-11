eceb487ca2d010c1b81e21ec691bd550345b0e51
Ariel Coppes <ariel.coppes@gmail.com>
2023-08-11 19:00:20 -0300

    fire projectiles and kill cars

---
+++ Abduction101/Assets/Abduction101/Controllers/BoltProjectileController.cs
@@ -0,0 +1,38 @@
+﻿using Abduction101.Systems;
+using Game.Components;
+using Gemserk.Leopotam.Ecs;
+using Gemserk.Leopotam.Ecs.Controllers;
+using UnityEngine;
+
+namespace Abduction101.Controllers
+{
+    public class BoltProjectileController : ControllerBase, IProjectileImpactEvent
+    {
+        // TODO: auto kill on impact || auto destroy on impact, etc
+        // in system.
+
+        public float damage;
+        
+        public void OnProjectileImpact(World world, Entity entity)
+        {
+            ref var health = ref entity.Get<HealthComponent>();
+            health.damages.Add(new DamageData()
+            {
+                // kill projectile
+                value = health.total
+            });
+            
+            // apply effect to targets nearby or to impact target 
+            var projectile = entity.Get<ProjectileComponent>();
+            // Debug.Log($"Collision with: {projectile.impactEntity}");
+
+            if (projectile.impactEntity.Exists())
+            {
+                projectile.impactEntity.Get<HealthComponent>().damages.Add(new DamageData()
+                {
+                    value = damage
+                });
+            }
+        }
+    }
+}
---
+++ Abduction101/Assets/Abduction101/Controllers/ProjectileImpactDetectionController.cs
@@ -0,0 +1,51 @@
+﻿using Game.Components;
+using Game.Utilities;
+using Gemserk.Leopotam.Ecs;
+using Gemserk.Leopotam.Ecs.Controllers;
+using Gemserk.Leopotam.Ecs.Events;
+using UnityEngine;
+
+namespace Abduction101.Controllers
+{
+    public class ProjectileImpactDetectionController : ControllerBase, IInit, IDestroyed
+    {
+        public void OnInit(World world, Entity entity)
+        {
+            var physics = entity.Get<PhysicsComponent>();
+            physics.collisionsEventsDelegate.onCollisionEnter += OnEntityCollision;
+        }
+        
+        public void OnDestroyed(World world, Entity entity)
+        {
+            var physics = entity.Get<PhysicsComponent>();
+            physics.collisionsEventsDelegate.onCollisionEnter -= OnEntityCollision;
+        }
+
+        private void OnEntityCollision(World world, Entity entity, IEntityCollisionDelegate.EntityCollision entityCollision)
+        {
+            // Debug.Log("COLLISION");
+
+            if (entity.Get<ProjectileComponent>().impacted)
+            {
+                return;
+            }
+
+            if (entityCollision.entity.Exists())
+            {
+                // could search for targets and perform some effect...
+                // entityCollision.entity.Get<HealthComponent>().damages.Add(new DamageData()
+                // {
+                //     value = 1
+                // });
+                
+                // entity.Get<HealthComponent>().damages.Add(new DamageData()
+                // {
+                //     value = 1000
+                // });
+
+                entity.Get<ProjectileComponent>().impacted = true;
+                entity.Get<ProjectileComponent>().impactEntity = entityCollision.entity;
+            }
+        }
+    }
+}
