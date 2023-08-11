31d717649fdc32ec882022fadb0107e5d3ed9ba5
Ariel Coppes <ariel.coppes@gmail.com>
2023-08-11 10:48:01 -0300

    explosions do damage

---
+++ Abduction101/Assets/Abduction101/Controllers/ExplosionController.cs
@@ -0,0 +1,49 @@
+﻿using Game.Components;
+using Game.Utilities;
+using Gemserk.Leopotam.Ecs;
+using Gemserk.Leopotam.Ecs.Components;
+using Gemserk.Leopotam.Ecs.Controllers;
+using Gemserk.Leopotam.Ecs.Events;
+using UnityEngine;
+
+namespace Abduction101.Controllers
+{
+    public class ExplosionController : ControllerBase, IUpdate
+    {
+        public Targeting targeting;
+        
+        public void OnUpdate(World world, Entity entity, float dt)
+        {
+            ref var states = ref entity.Get<StatesComponent>();
+            var position = entity.Get<PositionComponent>();
+
+            if (!states.HasState("Explosion"))
+            {
+                var targets = world.GetTargets(new RuntimeTargetingParameters()
+                {
+                    filter = targeting.targeting,
+                    direction = Vector3.right,
+                    position = position.value,
+                    player = entity.Get<PlayerComponent>().player
+                });
+
+                foreach (var target in targets)
+                {
+                    if (target.entity.Exists())
+                    {
+                        target.entity.Get<HealthComponent>().damages.Add(new DamageData()
+                        {
+                            value = 1,
+                            position = position.value,
+                            knockback = true,
+                            source = entity,
+                            vfxDefinition = null
+                        });
+                    }
+                }
+                
+                states.EnterState("Explosion");
+            }
+        }
+    }
+}
